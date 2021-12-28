2546a43b514094635512b6998d6a53b9b3e98e32
Marina <elfen_lied2@inbox.lv>
2021-12-28 02:46:58 +0200

    Auth via Google

---
+++ app/src/main/java/com/marinanitockina/angelsnails/AppViewModel.kt
@@ -0,0 +1,35 @@
+package com.marinanitockina.angelsnails
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class AppViewModel : ViewModel() {
+
+    val loadingState = MutableStateFlow(LoadingState.IDLE)
+
+    fun signInWithEmailAndPassword(email: String, password: String) = viewModelScope.launch {
+        try {
+            loadingState.emit(LoadingState.LOADING)
+            Firebase.auth.signInWithEmailAndPassword(email, password)
+            loadingState.emit(LoadingState.LOADED)
+        } catch (e: Exception) {
+            loadingState.emit(LoadingState.error(e.localizedMessage))
+        }
+    }
+
+    fun signWithCredential(credential: AuthCredential) = viewModelScope.launch {
+        try {
+            loadingState.emit(LoadingState.LOADING)
+            Firebase.auth.signInWithCredential(credential)
+            loadingState.emit(LoadingState.LOADED)
+        } catch (e: Exception) {
+            loadingState.emit(LoadingState.error(e.localizedMessage))
+        }
+    }
+
+}
