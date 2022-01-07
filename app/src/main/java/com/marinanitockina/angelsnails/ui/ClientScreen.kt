cc9d1ce720a6dab28f1ad1c6aae29b1dacaad213
Marina <elfen_lied2@inbox.lv>
2022-01-07 10:52:28 +0200

    Rendering service images

--- app/src/main/java/com/marinanitockina/angelsnails/ui/ClientScreen.kt
+++ app/src/main/java/com/marinanitockina/angelsnails/ui/ClientScreen.kt
@@ -1,18 +1,32 @@
 package com.marinanitockina.angelsnails.ui
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.marinanitockina.angelsnails.models.Service
 import com.marinanitockina.angelsnails.models.UserState
+import com.skydoves.landscapist.glide.GlideImage
 
 
 @Composable
 fun ClientScreen(userState: UserState, services: List<Service>) {
     LazyColumn() {
         items(items = services) { service ->
-            Text(text = service.name ?: "null")
+            ServiceItem(service = service)
         }
     }
+}
+
+@Composable
+fun ServiceItem(service: Service) {
+    GlideImage(imageModel = service.pictureUrl,
+    modifier = Modifier
+        .fillMaxWidth()
+        .height(80.dp))
+    Text(text = service.name ?: "null")
 }
