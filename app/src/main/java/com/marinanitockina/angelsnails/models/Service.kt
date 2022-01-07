4f4d662507bd5939e298ceeb0547cad8437cf336
Marina <elfen_lied2@inbox.lv>
2022-01-07 10:43:40 +0200

    Added pictureUrl attribute to service

--- app/src/main/java/com/marinanitockina/angelsnails/models/Service.kt
+++ app/src/main/java/com/marinanitockina/angelsnails/models/Service.kt
@@ -2,5 +2,6 @@ package com.marinanitockina.angelsnails.models
 
 data class Service(
     val name: String? = null,
-    val price: String? = null
+    val price: String? = null,
+    val pictureUrl: String? = null
 )
