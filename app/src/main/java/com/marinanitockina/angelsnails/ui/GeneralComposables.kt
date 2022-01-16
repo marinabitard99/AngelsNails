9adb1a8bdc953a5734dc4d3cd5a02ec9116cf132
Marina <elfen_lied2@inbox.lv>
2022-01-17 01:26:57 +0200

    Added separate chip button

--- app/src/main/java/com/marinanitockina/angelsnails/ui/GeneralComposables.kt
+++ app/src/main/java/com/marinanitockina/angelsnails/ui/GeneralComposables.kt
@@ -1,13 +1,14 @@
 package com.marinanitockina.angelsnails.ui
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,9 +40,36 @@ fun Chip(
     }
 }
 
+@Composable
+fun ChipButton(text: String, modifier: Modifier = Modifier, onClickAction: () -> Unit = {}) {
+    OutlinedButton(
+        onClick = { onClickAction() },
+        shape = RoundedCornerShape(25.dp),
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(backgroundColor = Pink100),
+        border = BorderStroke(1.dp, DarkPink)
+    ) {
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+            modifier = modifier,
+            color = DarkPink
+        )
+    }
+}
+
 @Preview("Chip")
 @Composable
 fun ChipPreview() {
     Chip(text = "25.00 €")
+}
+
+@Preview("ChipButton")
+@Composable
+fun ChipButtonPreview() {
+    Row {
+        ChipButton(text = "SELECT", modifier = Modifier.align(Alignment.CenterVertically))
+    }
+
 }
 
