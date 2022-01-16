a66af97de71a33927227de5fa04c08bf90d784ea
Marina <elfen_lied2@inbox.lv>
2022-01-16 04:53:36 +0200

    Master item now expands

--- app/src/main/java/com/marinanitockina/angelsnails/ui/ClientScreen.kt
+++ app/src/main/java/com/marinanitockina/angelsnails/ui/ClientScreen.kt
@@ -2,6 +2,7 @@ package com.marinanitockina.angelsnails.ui
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.BorderStroke
@@ -20,7 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,10 +39,6 @@ import com.marinanitockina.angelsnails.ui.theme.Pink100
 import com.marinanitockina.angelsnails.ui.theme.Pink50
 import com.skydoves.landscapist.ShimmerParams
 import com.skydoves.landscapist.glide.GlideImage
-import com.vanpra.composematerialdialogs.MaterialDialog
-import com.vanpra.composematerialdialogs.datetime.date.DatePickerDefaults
-import com.vanpra.composematerialdialogs.datetime.date.datepicker
-import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 import kotlinx.coroutines.launch
 import java.text.DateFormat
 import java.text.SimpleDateFormat
@@ -247,9 +243,25 @@ fun ServiceItem(service: Service) {
                 color = DarkPink,
                 modifier = Modifier.padding(start = 9.dp)
             )
-            LazyColumn(modifier = Modifier.height((service.masters.size * 70).dp)) {
+
+            val expandedItem: MutableState<String?> = remember { mutableStateOf(null) }
+
+            LazyColumn(
+                modifier = Modifier.height(
+                    if (expandedItem.value == null) {
+                        (service.masters.size * 70).dp
+                    } else {
+                        (service.masters.size * 70 + 95).dp
+                    }
+                )
+            ) {
                 items(items = service.masters.toList()) { master ->
-                    MasterCard(master = master.second)
+                    MasterCard(
+                        master = master.second,
+                        expanded = master.second.name == expandedItem.value,
+                        onSelectClicked = {
+                            expandedItem.value = it
+                        })
                 }
             }
         }
@@ -260,83 +272,227 @@ fun ServiceItem(service: Service) {
 }
 
 @Composable
-fun MasterCard(master: ServiceMaster = ServiceMaster()) {
+fun MasterCard(
+    master: ServiceMaster = ServiceMaster(),
+    expanded: Boolean = false,
+    onSelectClicked: (String?) -> Unit = {}
+) {
+
+    val height = animateIntAsState(if (expanded) 165 else 70)
+
     Card(
-        shape = RoundedCornerShape(100.dp),
+        shape = RoundedCornerShape(25.dp),
         modifier = Modifier
-            .height(70.dp)
+            .height(height.value.dp)
             .fillMaxWidth()
             .padding(vertical = 4.dp, horizontal = 9.dp),
         elevation = 2.dp,
         backgroundColor = Color.White
     ) {
-        Row(
-            modifier = Modifier.padding(vertical = 10.dp, horizontal = 20.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
+        Column(
+            modifier = Modifier.padding(vertical = 10.dp, horizontal = 20.dp)
         ) {
-
-            Row {
-                GlideImage(
-                    imageModel = master.pictureUrl,
+            Box {
+                Row(
                     modifier = Modifier
-                        .align(Alignment.CenterVertically)
-                        .aspectRatio(1f)
-                        .clip(CircleShape)
-                        .border(1.dp, DarkPink, CircleShape),
-                    shimmerParams = ShimmerParams(
-                        baseColor = Pink50,
-                        highlightColor = Pink100,
-                        durationMillis = 500,
-                        dropOff = 0.65f,
-                        tilt = 20f
-                    ),
-                )
-                Text(
-                    master.name!!,
-                    modifier = Modifier
-                        .align(Alignment.CenterVertically)
-                        .padding(start = 10.dp),
-                    style = MaterialTheme.typography.body1
-                )
-            }
-
-            val dialogState = rememberMaterialDialogState()
-            MaterialDialog(
-                dialogState = dialogState,
-                buttons = {
-                    positiveButton(text = "Ok", textStyle = TextStyle(color = DarkPink))
-                    negativeButton(text ="Cancel", textStyle = TextStyle(color = DarkPink))
-                }
-            ) {
-                datepicker(
-                    yearRange = IntRange(2022, 2022),
-                    colors = DatePickerDefaults.colors(
-                        headerBackgroundColor = Pink100,
-                        headerTextColor = DarkPink,
-                        activeBackgroundColor = Pink100,
-                        inactiveBackgroundColor = Color.Transparent,
-                        activeTextColor = DarkPink,
-                        inactiveTextColor = MaterialTheme.colors.onBackground
+                        .align(Alignment.TopStart)
+                        .fillMaxWidth()
+                ) {
+                    GlideImage(
+                        imageModel = master.pictureUrl,
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .clip(CircleShape)
+                            .border(1.dp, DarkPink, CircleShape)
+                            .width(43.dp)
+                            .height(43.dp),
+                        shimmerParams = ShimmerParams(
+                            baseColor = Pink50,
+                            highlightColor = Pink100,
+                            durationMillis = 500,
+                            dropOff = 0.65f,
+                            tilt = 20f
+                        ),
                     )
-                ) { date ->
-                    // Do stuff with java.time.LocalDate object which is passed in
+                    Text(
+                        master.name!!,
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .padding(start = 10.dp),
+                        style = MaterialTheme.typography.body1
+                    )
+                }
+
+                OutlinedButton(
+                    onClick = { if (expanded) onSelectClicked(null) else onSelectClicked(master.name) },
+                    shape = RoundedCornerShape(25.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Pink100),
+                    border = BorderStroke(1.dp, DarkPink),
+                    modifier = Modifier.align(Alignment.TopEnd)
+                ) {
+                    Text(
+                        text = if (expanded) "HIDE" else "SELECT",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                        color = DarkPink
+                    )
                 }
             }
 
-            OutlinedButton(
-                onClick = { dialogState.show() },
-                shape = RoundedCornerShape(25.dp),
-                modifier = Modifier.align(Alignment.CenterVertically),
-                colors = ButtonDefaults.buttonColors(backgroundColor = Pink100),
-                border = BorderStroke(1.dp, DarkPink)
+            AnimatedVisibility(
+                visible = expanded,
+                enter = expandVertically(),
+                exit = shrinkVertically(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp)
             ) {
-                Text(
-                    text = "SELECT",
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.align(Alignment.CenterVertically),
-                    color = DarkPink
+
+                Divider(
+                    Modifier.height(1.dp),
+                    color = Color.LightGray
                 )
+
+                Column {
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 10.dp)
+                    ) {
+                        Text(
+                            text = "Date 15.01.2022",
+                            style = MaterialTheme.typography.body1,
+                            color = DarkPink,
+                            modifier = Modifier
+                                .align(Alignment.TopStart),
+                            fontSize = 18.sp
+                        )
+
+                        Text(
+                            text = "Time 15:00",
+                            style = MaterialTheme.typography.body1,
+                            color = DarkPink,
+                            modifier = Modifier
+                                .align(Alignment.TopEnd),
+                            fontSize = 18.sp
+                        )
+
+                    }
+
+                    OutlinedButton(
+                        onClick = { },
+                        shape = RoundedCornerShape(25.dp),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Pink100),
+                        border = BorderStroke(1.dp, DarkPink),
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(top = 5.dp)
+                    ) {
+                        Text(
+                            text = "FINISH",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(Alignment.CenterVertically),
+                            color = DarkPink
+                        )
+                    }
+                }
+
             }
+
+
+//
+//            val dateDialogState = rememberMaterialDialogState()
+//            val timeDialogState = rememberMaterialDialogState()
+//            val confirmDialogState = rememberMaterialDialogState()
+//
+//            Row(
+//                modifier = Modifier.padding(vertical = 10.dp, horizontal = 20.dp),
+//                horizontalArrangement = Arrangement.SpaceBetween
+//            ) {
+//                val calendar = Calendar.getInstance()
+//                calendar.add(Calendar.DATE, 1)
+//                val df = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+//                val formattedDate = df.format(calendar.time)
+//                Text("Date: ")
+//                TextButton(onClick = { /*TODO*/ }) {
+//                    Text(formattedDate)
+//                }
+//                val availableTimes = listOf(
+//                    "9:00", "10:00", "11:00", "12:00", "13:00",
+//                    "14:00", "15:00", "16:00", "17:00", "18:00"
+//                )
+//                val shownTime = remember { availableTimes.random() }
+//                Text("Time: ")
+//                TextButton(onClick = {/*TODO*/ }) {
+//                    Text(shownTime)
+//                }
+//            }
+
+//            val dateDialogState = rememberMaterialDialogState()
+//            MaterialDialog(
+//                dialogState = dialogState,
+//                buttons = {
+//                    positiveButton(text = "Ok", textStyle = TextStyle(color = DarkPink)) {
+//                        val t
+//                    }
+//                    negativeButton(text ="Cancel", textStyle = TextStyle(color = DarkPink))
+//                }
+//            ) {
+//                datepicker(
+//                    yearRange = IntRange(2022, 2022),
+//                    colors = DatePickerDefaults.colors(
+//                        headerBackgroundColor = Pink100,
+//                        headerTextColor = DarkPink,
+//                        activeBackgroundColor = Pink100,
+//                        inactiveBackgroundColor = Color.Transparent,
+//                        activeTextColor = DarkPink,
+//                        inactiveTextColor = MaterialTheme.colors.onBackground
+//                    )
+//                ) { date ->
+//                    // Do stuff with java.time.LocalDate object which is passed in
+//                }
+//            }
+
+//            val dateAndTimeDialogState = rememberMaterialDialogState()
+//            MaterialDialog(
+//                dialogState = dateAndTimeDialogState,
+//                buttons = {
+//                    positiveButton("Finish", textStyle = TextStyle(color = DarkPink))
+//                    negativeButton("Cancel", textStyle = TextStyle(color = DarkPink))
+//                }
+//            ) {
+//                title(text = "Select date and time")
+//
+//                customView {
+//                    val dateDialogState = rememberMaterialDialogState()
+//                    MaterialDialog(
+//                        dialogState = dialogState,
+//                        buttons = {
+//                            positiveButton(text = "Ok", textStyle = TextStyle(color = DarkPink))
+//                            negativeButton(text = "Cancel", textStyle = TextStyle(color = DarkPink))
+//                        }
+//                    ) {
+//                        datepicker(
+//                            yearRange = IntRange(2022, 2022),
+//                            colors = DatePickerDefaults.colors(
+//                                headerBackgroundColor = Pink100,
+//                                headerTextColor = DarkPink,
+//                                activeBackgroundColor = Pink100,
+//                                inactiveBackgroundColor = Color.Transparent,
+//                                activeTextColor = DarkPink,
+//                                inactiveTextColor = MaterialTheme.colors.onBackground
+//                            )
+//                        ) { date ->
+//                            // Do stuff with java.time.LocalDate object which is passed in
+//                        }
+//                    }
+//
+
+//
+//                }
+//            }
+
         }
     }
 }
